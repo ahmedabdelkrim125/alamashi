0174215a42e793f2605442e2e10fd5ba88e55f77
ahmedabdelkrim125 <ahmed125abdelkrim@gmail.com>
2025-08-12 14:44:52 +0300

    done logic splash_screen

--- lib/features/splash/presentation/views/Widgets/splash_screen_body.dart
+++ lib/features/splash/presentation/views/Widgets/splash_screen_body.dart
@@ -1,10 +1,56 @@
+import 'package:egyptian_supermaekat/core/app_router.dart';
 import 'package:egyptian_supermaekat/core/utils/app_images.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
 
-class SplashScreenBody extends StatelessWidget {
+class SplashScreenBody extends StatefulWidget {
   const SplashScreenBody({super.key});
+
+  @override
+  State<SplashScreenBody> createState() => _SplashScreenBodyState();
+}
+
+class _SplashScreenBodyState extends State<SplashScreenBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+  @override
+  void initState() {
+    _initAnimations();
+    _navigateToonBoarding();
+  }
+
+  void _navigateToonBoarding() {
+    Future.delayed(
+      const Duration(seconds: 4),
+      () {
+        context.push(AppRouter.onboarding);
+      },
+    );
+  }
+
+  void _initAnimations() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+    super.initState();
+    _fadeAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+    _scaleAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeOut),
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +59,20 @@ class SplashScreenBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Spacer(flex: 3),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset(
-              Assets.imagesLeafyChat,
-              width: 250.sp,
+        FadeTransition(
+          opacity: _fadeAnimation,
+          child: ScaleTransition(
+            scale: _scaleAnimation,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  Assets.imagesLeafyChat,
+                  width: 250.sp,
+                ),
+              ],
             ),
-          ],
+          ),
         ),
         Spacer(flex: 2),
         SvgPicture.asset(
