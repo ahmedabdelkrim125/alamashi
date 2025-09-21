0e82f038afef350e101ab6cd988a0178333cb7d7
ahmedabdelkrim125 <ahmed125abdelkrim@gmail.com>
2025-09-22 00:02:40 +0300

    sloving build.gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,17 +1,7 @@
-plugins {
-    id("com.android.application")
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
 android {
     namespace = "com.example.egyptian_supermaekat_"
     compileSdk = flutter.compileSdkVersion
-     ndkVersion = "27.0.12077973"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -23,10 +13,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.egyptian_supermaekat_"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23 
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -35,13 +22,12 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-}
 
-flutter {
-    source = "../.."
+    // ✅ أضف هذا الجزء
+    buildFeatures {
+        buildConfig true
+    }
 }
